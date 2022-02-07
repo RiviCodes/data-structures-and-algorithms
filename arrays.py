4b598d99d70f9497eb84594637b951ed3d631ec4
RiviCodes <jose.riveramz@udlap.mx>
2022-02-06 22:44:32 -0600

    Finalize code's block

--- arrays.py
+++ arrays.py
@@ -1,3 +1,17 @@
 new_list = [1, 2, 3]
 result = new_list[0]
 
+if 1 in new_list: print(True)
+
+for n in new_list:
+  if n == 1:
+    print(True)
+
+    break
+
+numbers = [4, 5, 6]
+
+new_list.extend(numbers)
+'''Prints [1, 2, 3, 4, 5 , 6]'''
+
+
